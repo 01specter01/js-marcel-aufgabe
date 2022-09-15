f7cd8ea6c8546552b4631bd669c83c4bfec10be2
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-15 10:26:43 +0200

    done

---
+++ marcel.js
@@ -0,0 +1,18 @@
+console.log("Zeitmaße Umwandeln");
+function zeitConverter(fromType, toType, time) {
+    if (fromType == "millisecond" && toType == "minute") {
+        let newMinutes = time / 60000;
+        return newMinutes;
+    } else if (fromType == "minute" && toType == "millisecond") {
+        let newMilisec = time * 60000;
+        return newMilisec;
+    } else if (fromType == "hour" && toType == "millisecond") {
+        let newMilisec = time * 3600000;
+        return newMilisec;
+    } else {
+        return false;
+    }
+}
+console.log(zeitConverter("millisecond", "minute", 120000)); // => 2
+console.log(zeitConverter("minute", "millisecond", 2)); // => 120000
+console.log(zeitConverter("hour", "millisecond", 1)); // => 3600000
